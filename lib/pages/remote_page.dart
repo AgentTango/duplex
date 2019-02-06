9a3ae7723a6c5810ed0e1d6eca727cc7dc583987
rshrc <rishieric91@gmail.com>
2019-02-06 22:45:19 +0530

    Added the Video Player

--- lib/pages/remote_page.dart
+++ lib/pages/remote_page.dart
@@ -5,17 +5,37 @@ class RemotePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Icon(
-              Icons.play_circle_outline,
-              color: Colors.greenAccent,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: IconButton(
+                icon: Icon(
+                  Icons.play_circle_outline,
+                  color: Colors.greenAccent,
+                  size: 64.0,
+                ),
+                onPressed: (){
+                  // TODO
+                },
+              ),
             ),
-            Icon(
-              Icons.pause_circle_outline,
-              color: Colors.red,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: IconButton(
+                icon: Icon(
+                  Icons.pause_circle_outline,
+                  color: Colors.red,
+                  size: 64.0,
+                ),
+                onPressed: (){
+                  // TODO
+                },
+              ),
             ),
+
           ],
         ),
       ),
