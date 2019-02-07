255e658788b403cb4aab2460b54de370c0e46027
Aditya Thakur <aditya1999thakur@gmail.com>
2019-02-08 01:55:26 +0530

    Tweaked UI of newRemotePage [2/?]

--- lib/pages/newRemotePage.dart
+++ lib/pages/newRemotePage.dart
@@ -15,7 +15,6 @@ class RemotePageState extends State<RemotePage> {
         .of(context)
         .size;
     return Scaffold(
-      backgroundColor: Colors.white,
       body: Column(
         children: <Widget>[
           InkWell(
@@ -24,43 +23,54 @@ class RemotePageState extends State<RemotePage> {
               height: screenSize.height * 0.6,
               width: screenSize.width,
               alignment: Alignment.center,
-              child: Text("This is where the thumbnail stuff goes"),
+              child: Icon(Icons.music_video,color: Colors.indigoAccent[700],size: 160.0,),
             ),
             onTap: () {},
             enableFeedback: true,
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                RawMaterialButton(
-                  onPressed: () {},
-                  fillColor: Colors.white,
-                  splashColor: Colors.indigoAccent,
-                  highlightElevation: 0.0,
-                  elevation: 0.0,
-                  child: Icon(Icons.skip_previous,color: Colors.indigoAccent,size: 60.0,),
-                  shape: CircleBorder(),
-                ),
-                RawMaterialButton(
-                  onPressed: () {},
-                  fillColor: Colors.indigoAccent,
-                  child: Icon(Icons.play_arrow,color: Colors.white,size: 80.0,),
-                  shape: CircleBorder(),
-                ),
-                RawMaterialButton(
-                  onPressed: () {},
-                  fillColor: Colors.white,
-                  splashColor: Colors.indigoAccent,
-                  elevation: 0.0,
-                  highlightElevation: 0.0,
-                  child: Icon(Icons.skip_next,color: Colors.indigoAccent,size: 60.0,),
-                  shape: CircleBorder(),
-                ),
-              ],
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              alignment: Alignment.center,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  RawMaterialButton(
+                    onPressed: () {},
+                    fillColor: Colors.white,
+                    splashColor: Colors.indigoAccent,
+                    highlightElevation: 0.0,
+                    elevation: 0.0,
+                    child: Icon(Icons.skip_previous,color: Colors.indigoAccent,size: 60.0,),
+                    shape: CircleBorder(),
+                  ),
+                  RawMaterialButton(
+                    onPressed: () {},
+                    fillColor: Colors.indigoAccent,
+                    splashColor: Colors.indigoAccent[700],
+                    child: Icon(Icons.play_arrow,color: Colors.white,size: 80.0,),
+                    shape: CircleBorder(),
+                  ),
+                  RawMaterialButton(
+                    onPressed: () {},
+                    fillColor: Colors.white,
+                    splashColor: Colors.indigoAccent,
+                    elevation: 0.0,
+                    highlightElevation: 0.0,
+                    child: Icon(Icons.skip_next,color: Colors.indigoAccent,size: 60.0,),
+                    shape: CircleBorder(),
+                  ),
+                ],
+              ),
             ),
           ),
+          Container(
+            width: screenSize.width,
+            height: screenSize.height*0.07,
+            color: Colors.indigoAccent,
+            alignment: Alignment.center,
+            child: Text("Back to Browsing",style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w500),),
+          )
         ],
       ),
     );
